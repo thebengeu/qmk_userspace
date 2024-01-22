65bdd2fa5877593741b45ab18cbc71ead67f058d
Beng Eu <beng@bengeu.com>
2024-01-22 13:42:41 +0800

    feat: add cut, copy and paste combos

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -60,34 +60,31 @@ const key_override_t capsword_key_override = ko_make_basic(MOD_MASK_SHIFT, CW_TO
 
 const key_override_t **key_overrides = (const key_override_t *[]){&capsword_key_override, NULL};
 
-// thumb combos
-
-#if defined(MIRYOKU_KLUDGE_THUMBCOMBOS)
 const uint16_t PROGMEM thumbcombos_base_right[] = {LT(U_SYM, KC_ENT), LT(U_NUM, KC_BSPC), COMBO_END};
 const uint16_t PROGMEM thumbcombos_base_left[]  = {LT(U_NAV, KC_SPC), LT(U_MOUSE, KC_TAB), COMBO_END};
 const uint16_t PROGMEM thumbcombos_nav[]        = {KC_ENT, KC_BSPC, COMBO_END};
 const uint16_t PROGMEM thumbcombos_mouse[]      = {KC_BTN2, KC_BTN1, COMBO_END};
 const uint16_t PROGMEM thumbcombos_media[]      = {KC_MSTP, KC_MPLY, COMBO_END};
 const uint16_t PROGMEM thumbcombos_num[]        = {KC_0, KC_MINS, COMBO_END};
-#    if defined(MIRYOKU_LAYERS_FLIP)
-const uint16_t PROGMEM thumbcombos_sym[] = {KC_UNDS, KC_LPRN, COMBO_END};
-#    else
-const uint16_t PROGMEM thumbcombos_sym[] = {KC_RPRN, KC_UNDS, COMBO_END};
-#    endif
-const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
+const uint16_t PROGMEM thumbcombos_sym[]        = {KC_UNDS, KC_LPRN, COMBO_END};
+const uint16_t PROGMEM thumbcombos_fun[]        = {KC_SPC, KC_TAB, COMBO_END};
+const uint16_t PROGMEM thumbcombos_cut[]        = {LT(U_BUTTON, KC_Z), KC_X, COMBO_END};
+const uint16_t PROGMEM thumbcombos_copy[]       = {LT(U_BUTTON, KC_Z), KC_C, COMBO_END};
+const uint16_t PROGMEM thumbcombos_paste[]      = {LT(U_BUTTON, KC_Z), KC_V, COMBO_END};
+
+// clang-format off
 combo_t                key_combos[]      = {COMBO(thumbcombos_base_right, LT(U_FUN, KC_DEL)),
                                             COMBO(thumbcombos_base_left, LT(U_MEDIA, KC_ESC)),
                                             COMBO(thumbcombos_nav, KC_DEL),
                                             COMBO(thumbcombos_mouse, KC_BTN3),
                                             COMBO(thumbcombos_media, KC_MUTE),
-                                            COMBO(thumbcombos_num, KC_DOT),
-#    if defined(MIRYOKU_LAYERS_FLIP)
-                        COMBO(thumbcombos_sym, KC_RPRN),
-#    else
-                        COMBO(thumbcombos_sym, KC_LPRN),
-#    endif
-                                            COMBO(thumbcombos_fun, KC_APP)};
-#endif
+                                            COMBO(thumbcombos_num, MEH_T(KC_DOT)),
+                                            COMBO(thumbcombos_sym, KC_RPRN),
+                                            COMBO(thumbcombos_fun, KC_APP),
+                                            COMBO(thumbcombos_cut, U_CUT),
+                                            COMBO(thumbcombos_copy, U_CPY),
+                                            COMBO(thumbcombos_paste, U_PST)};
+// clang-format on
 
 bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
