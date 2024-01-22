8ab57948d217a78eb695635c7cc0a7f374c01899
Beng Eu <beng@bengeu.com>
2024-01-23 07:21:25 +0800

    feat: remove tap dance and key override, clean up unused Miryoku bits

--- keyboards/crkbd/keymaps/thebengeu/rules.mk
+++ keyboards/crkbd/keymaps/thebengeu/rules.mk
@@ -1,7 +1,7 @@
 LTO_ENABLE = yes
-OLED_ENABLE = no
 PS2_DRIVER = interrupt
 PS2_ENABLE = yes
 PS2_MOUSE_ENABLE = yes
 RGBLIGHT_ENABLE = no
 USER_NAME := manna-harbour_miryoku
+WPM_ENABLE = yes
--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -7,33 +7,13 @@
 
 #include "custom_config.h"
 
-// default but used in macros
-#undef TAPPING_TERM
-#define TAPPING_TERM 150
-
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM_PER_KEY
-
-// Auto Shift
-#define NO_AUTO_SHIFT_ALPHA
-#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-#define AUTO_SHIFT_NO_SETUP
-
-// Mouse key speed and acceleration.
-#undef MOUSEKEY_DELAY
+#define COMBO_TERM 25
+#define EXTRA_SHORT_COMBOS
 #define MOUSEKEY_DELAY          0
-#undef MOUSEKEY_INTERVAL
 #define MOUSEKEY_INTERVAL       16
-#undef MOUSEKEY_WHEEL_DELAY
-#define MOUSEKEY_WHEEL_DELAY    0
-#undef MOUSEKEY_MAX_SPEED
 #define MOUSEKEY_MAX_SPEED      6
-#undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX    64
-
-// Thumb Combos
-#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
-  #define COMBO_TERM 25
-  #define EXTRA_SHORT_COMBOS
-#endif
+#define MOUSEKEY_WHEEL_DELAY    0
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM_PER_KEY
+#define TAPPING_TERM 150
