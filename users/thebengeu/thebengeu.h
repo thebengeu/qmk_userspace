00e2344f47e199baeae019d5c8e99bcaf901a5f9
Beng Eu <beng@bengeu.com>
2024-02-15 21:57:37 +0800

    refactor: replace U_NA and U_NU with KC_NO

--- users/thebengeu/thebengeu.h
+++ users/thebengeu/thebengeu.h
@@ -5,10 +5,6 @@ enum custom_keycodes {
   SHIFT_GUI_TAB
 };
 
-#define U_NP KC_NO // key is not present
-#define U_NA KC_NO // present but not available for use
-#define U_NU KC_NO // available but not used
-
 #define U_RDO SCMD(KC_Z)
 #define U_PST LCMD(KC_V)
 #define U_CPY LCMD(KC_C)
