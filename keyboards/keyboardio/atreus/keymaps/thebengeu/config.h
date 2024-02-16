975e5d77159e54bd5e479468fbabedb9a496c82b
Beng Eu <beng@bengeu.com>
2024-02-16 23:52:58 +0800

    feat: realign and renumber atreus layout

--- keyboards/keyboardio/atreus/keymaps/thebengeu/config.h
+++ keyboards/keyboardio/atreus/keymaps/thebengeu/config.h
@@ -6,11 +6,11 @@
 K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
-          K32, K33, K34,                K35, K36, K37            \
+               K30, K31, K32,      K33, K34, K35                 \
 ) \
 LAYOUT( \
 K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24, XXX,      XXX, K25, K26, K27, K28, K29, \
-XXX, XXX, XXX, K32, K33, K34,      K35, K36, K37, XXX, XXX, XXX  \
+XXX, XXX, XXX, K30, K31, K32,      K33, K34, K35, XXX, XXX, XXX  \
 )
