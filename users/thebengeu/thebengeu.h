9d999ecf3bcd48c51a2e818ef00b6b9a650e255d
Beng Eu <beng@bengeu.com>
2025-01-18 01:12:54 +0800

    refactor: use enum for layer references

--- users/thebengeu/thebengeu.h
+++ users/thebengeu/thebengeu.h
@@ -1,25 +1,42 @@
 #pragma once
 
-// clang-format off
 enum custom_keycodes {
-  ALT_TAB = SAFE_RANGE,
-  CTL_4,
-  CTL_5,
-  CTL_6,
-  GUI_4,
-  GUI_5,
-  GUI_6,
-  GUI_TAB,
-  LLOCK,
-  NW_COLN,
-  NW_DLR,
-  NW_TOGG,
-  RCBR,
-  SHIFT_ALT_TAB,
-  SHIFT_GUI_TAB,
-  UNDS
+    ALT_TAB = SAFE_RANGE,
+    CTL_4,
+    CTL_5,
+    CTL_6,
+    GUI_4,
+    GUI_5,
+    GUI_6,
+    GUI_TAB,
+    LLOCK,
+    NW_COLN,
+    NW_DLR,
+    NW_TOGG,
+    RCBR,
+    SHIFT_ALT_TAB,
+    SHIFT_GUI_TAB,
+    UNDS,
 };
-// clang-format on
+
+enum layers {
+    _MAC,
+    _AUTO,
+    _WIN,
+    _S,
+    _MM,
+    _MW,
+    _NM,
+    _NW,
+    _SYM,
+    _FUN,
+    _MEH,
+    _HYPR,
+    _LCSG,
+    _MNO,
+    _HNO,
+    _KEYBALL,
+};
 
 #define LCSG(kc) (QK_LCTL | QK_LSFT | QK_LGUI | (kc))
 #define LSAG(kc) (QK_LSFT | QK_LALT | QK_LGUI | (kc))
