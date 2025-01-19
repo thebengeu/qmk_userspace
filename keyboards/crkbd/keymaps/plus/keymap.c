95e2b4c15670a14f1b7d709ba2cc4c6c6f08d94c
Beng Eu <beng@bengeu.com>
2025-01-19 11:44:14 +0800

    fix: remove keyball44 from qmk.json and fix other keyboards

--- keyboards/crkbd/keymaps/plus/keymap.c
+++ keyboards/crkbd/keymaps/plus/keymap.c
@@ -1,27 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    // clang-format off
-    [0] = {ENCODER_CCW_CW(KC_WH_U, KC_WH_D), ENCODER_CCW_CW(LSG(KC_LBRC),   LSG(KC_RBRC))},
-    [1] = {ENCODER_CCW_CW(KC_WH_U, KC_WH_D), ENCODER_CCW_CW(S(C(KC_TAB)), C(KC_TAB))},
-    [2] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(QK_AREP, QK_REP)},
-    [3] = {ENCODER_CCW_CW(KC_WH_L, KC_WH_R), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [4] = {ENCODER_CCW_CW(KC_WH_L, KC_WH_R), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [5] = {ENCODER_CCW_CW(G(KC_MINS), G(KC_EQL)), ENCODER_CCW_CW(KC_U, C(KC_R))},
-    [6] = {ENCODER_CCW_CW(C(KC_MINS), C(KC_EQL)), ENCODER_CCW_CW(KC_U, C(KC_R))},
-    [7] = {ENCODER_CCW_CW(KC_UP, KC_DOWN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN)},
-    [8] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_F1, KC_F2)},
-    [9] = {ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [10] = {ENCODER_CCW_CW(S(KC_H), S(KC_L)), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [11] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [12] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [13] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    [14] = {ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO)},
-    // clang-format on
-};
-#endif
-
 // void caps_word_set_user(bool active) {
 //     pimoroni_trackball_set_rgbw(active ? 0xff : 0, 0, 0, 0);
 // }
