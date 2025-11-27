0271b0b6a399d2225f4c8f4a95723371559bb347
Beng Eu <beng@bengeu.com>
2025-11-27 11:26:37 +0800

    chore(stront): make extra keys consistent with latest layout

--- keyboards/stront/keymaps/thebengeu/config.h
+++ keyboards/stront/keymaps/thebengeu/config.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define LSF LSFT_T(KC_LBRC)
+#define RSF RSFT_T(KC_RBRC)
+
 #define XXX KC_NO
 
 // clang-format off
@@ -12,7 +15,7 @@ K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
 LAYOUT( \
           K01, K02, K03, K04,      K05, K06, K07, K08,           \
 K00, K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, K09, \
-K20, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, K29, \
+LSF, K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, RSF, \
                K30, K31, K32,      K33, K34, K35                 \
 )
 // clang-format on
